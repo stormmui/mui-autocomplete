dc6198cd97260052ef3016bb0c707e8cc73af164
Michael Angerman <stormasm@gmail.com>
2018-04-27 11:59:47 -0700

    all 3 autocomplete demos are up and running

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import Demo from './demo';
+//import Demo from './IntegrationAutosuggest';
+import Demo from './IntegrationDownshift';
+//import Demo from './IntegrationReactSelect';
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {
   render(<Demo />, rootElement);
 }
-      
