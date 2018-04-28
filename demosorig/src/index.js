22ff3ea1819956bc48d4ebbcdd05aebdd61975db
Michael Angerman <stormasm@gmail.com>
2018-04-27 19:44:34 -0700

    move directory one level down

--- demosorig/src/index.js
+++ demosorig/src/index.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-//import Demo from './IntegrationAutosuggest';
-import Demo from './IntegrationDownshift';
+import Demo from './IntegrationAutosuggest';
+//import Demo from './IntegrationDownshift';
 //import Demo from './IntegrationReactSelect';
 
 const rootElement = document.querySelector('#root');
