c086814e1ecdad03f0438de90fd9c3e550bb043b
Michael Angerman <stormasm@gmail.com>
2018-04-30 13:22:22 -0700

    remove unused code

--- gridlist/src/index.js
+++ gridlist/src/index.js
@@ -2,8 +2,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Demo from './JsonAutosuggest';
-//import Demo from './JsonDownshift';
-//import Demo from './JsonReactSelect';
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {
